d4df77e6394458f75f6d5660f39fb3346149ac33
robert-ryu7 <balcerowicz87@gmail.com>
2021-12-10 14:34:48 +0100

    1 file modified

--- revolver/Revolver.tsx
+++ revolver/Revolver.tsx
@@ -25,7 +25,6 @@ function Revolver<T>({
   itemsGap = 0,
   children,
 }: RevolverProps<T>) {
-  const [currentIndex, setCurrentIndex] = React.useState(0);
   const revolverRef = React.useRef<HTMLDivElement>();
   const [revolverWidth, setRevolverWidth] = React.useState<number | null>(null);
   const [panInfo, setPanInfo] = React.useState<{
@@ -35,7 +34,6 @@ function Revolver<T>({
   const itemDistance = itemWidth + itemsGap;
   const renderedItemsCount = Math.ceil(revolverWidth / itemDistance) + 2;
   const wingItemsCount = Math.floor(renderedItemsCount / 2);
-  const firstIndex = currentIndex - wingItemsCount;
 
   const handlePanEnd = React.useCallback<HammerListener>(() => {
     setPanInfo(null);
@@ -60,43 +58,47 @@ function Revolver<T>({
 
   return (
     <React.Fragment>
-      <Motion
-        defaultStyle={{ x: 0 }}
-        style={{ x: spring(panInfo?.deltaX ?? 0) }}
+      <div
+        className="revolver"
+        style={{ height: itemHeight }}
+        ref={revolverRef}
       >
-        {({ x }) => (
-          <div
-            className="revolver"
-            style={{ height: itemHeight }}
-            ref={revolverRef}
-          >
-            <div className="revolver__origin">
-              {Array.from({ length: renderedItemsCount }).map((_, index) => {
-                const key = firstIndex + index;
-                const dX = itemDistance * key;
-                const itemIndex = wrapIndex(key, items.length);
-                const item = items[itemIndex];
+        <Motion
+          defaultStyle={{ x: 0 }}
+          style={{ x: spring(panInfo?.deltaX ?? 0) }}
+        >
+          {({ x }) => {
+            const middleItemIndex = Math.round(-x / itemDistance);
+            const firstIndex = middleItemIndex - wingItemsCount;
 
-                return (
-                  <Hammer onPanEnd={handlePanEnd} onPan={handlePan}>
-                    <div
-                      key={key}
-                      className="revolver__item"
-                      style={{
-                        width: itemWidth,
-                        left: itemWidth / -2,
-                        transform: `translate3d(${x + dX}px,0,0)`,
-                      }}
-                    >
-                      {children(item)}
-                    </div>
-                  </Hammer>
-                );
-              })}
-            </div>
-          </div>
-        )}
-      </Motion>
+            return (
+              <div className="revolver__origin">
+                {Array.from({ length: renderedItemsCount }).map((_, index) => {
+                  const key = firstIndex + index;
+                  const dX = itemDistance * key;
+                  const itemIndex = wrapIndex(key, items.length);
+                  const item = items[itemIndex];
+
+                  return (
+                    <Hammer key={key} onPanEnd={handlePanEnd} onPan={handlePan}>
+                      <div
+                        className="revolver__item"
+                        style={{
+                          width: itemWidth,
+                          left: itemWidth / -2,
+                          transform: `translate3d(${x + dX}px,0,0)`,
+                        }}
+                      >
+                        {children(item)}
+                      </div>
+                    </Hammer>
+                  );
+                })}
+              </div>
+            );
+          }}
+        </Motion>
+      </div>
       <pre>{JSON.stringify({ panInfo, revolverWidth })}</pre>
     </React.Fragment>
   );
